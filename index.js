ac599973c52914d3160d2833c7a63a0107ccac0d
Prakharchaudhary <99970010+Prakharchaudhary@users.noreply.github.com>
2023-11-03 18:28:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+require("dotenv").config()
+
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express()
+app.use(cors({
+    credentials:true
+}));
+
+
+require('./models/index')
+
+app.use(express.json());
+
+
+app.use(express.static('public'))
+
+const multer = require('multer');
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.join(__dirname,'./public/image'))
+
+    },
+
+
+    filename:function(req,file,cb){
+        const name = Date.now()+'-'+file.originalname
+        cb(null,name)
+
+    }
+})
+
+const filefilter = (req,file,cb)=>{
+    (file.mimetype == 'image/jpeg'  ||file.mimetype == 'image/png'  )?
+    cb(null,true):cb(null,false)
+}
+    
+ const upload = multer({storage :storage,
+    filefilter:filefilter})
+
+    const bodyParser = require('body-parser')
+    app.use(bodyParser.json())
+
+
+    app.get('/api', (req, res) => {
+        res.send(' hello world')
+    })
+    const {isAuthorize} = require('./middlewares/auth')
+
+
+    var userController = require('./controllers/userController');
+    var adminController = require('./controllers/adminController');
+
+
+    // users API
+      app.post("/register",userController.signupUsers);
+      app.post("/resendEmail",userController.resendEmail);
+      app.post("/verifyOTP/:id",userController.verifyOTP);
+      app.post("/savePassword",userController.savePassword);
+      app.get("/loginUser",userController.loginUser);
+      app.post("/createAddress/",isAuthorize,userController.createAddress);
+      app.post("/updateAddress/",isAuthorize,userController.updateAddress);
+      app.get("/getAddressByUserId/",isAuthorize,userController.getAddressByUserId);
+      app.post("/addToCart",isAuthorize,userController.addToCart);
+      app.get("/getUserCart",isAuthorize,userController.getUserCart);
+      app.post("/updateCartItem",isAuthorize,userController.updateCartItem);
+      app.post("/deleteCartItem",isAuthorize,userController.deleteCartItem);
+      app.post("/userLogout",isAuthorize,userController.userLogout);
+
+      
+
+      // admin API
+      app.post("/addCategory",adminController.addCategory);
+      app.get("/getAllCategories",adminController.getAllCategories);
+      app.get("/getCategoryById/:id",adminController.getCategoryById);
+      app.post("/updateCategory/:id",adminController.updateCategory);
+      app.post("/deleteCategory/:id",adminController.deleteCategory);
+      app.post("/addBanner",upload.fields([{ name: 'image1' }, { name: 'image2' }, { name: 'image3' }]),adminController.addBanner);
+      app.post("/updateBanner",upload.fields([{ name: 'image1' }, { name: 'image2' }, { name: 'image3' }]),adminController.updateBanner);
+      app.get("/getBanner",adminController.getBanner);
+      app.post("/addProduct/:id",upload.single('image'),adminController.addProduct);
+      app.get("/getProductbyID/:id",adminController.getProductbyID);
+      app.get("/getProductbyCategoryID/:category_id",adminController.getProductbyCategoryID);
+      app.post("/updateProduct/:id",adminController.updateProduct); 
+      app.post("/deleteProduct/:id",adminController.deleteProduct);
+
+    
+    const PORT = process.env.PORT || 7000
+    app.listen(PORT, () => {
+        console.log('app will running on port 7000 ');
+    })
---
+++ models/index.js
@@ -0,0 +1,47 @@
+const { Sequelize,DataTypes } = require('sequelize');
+const sequelize = new Sequelize('Divine', 'root', 'cprakhar999@gmail.com', {
+    host: 'localhost',
+    logging:false,
+    dialect: 'mysql',
+    // operatorsAliases: false,
+  
+  });
+  sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+  const db = {}
+  db.Sequelize=Sequelize
+  db.sequelize = sequelize
+
+  db.userModels = require('./userModels')(sequelize,DataTypes)
+  db.category = require('./category')(sequelize,DataTypes)
+  db.banner = require('./banner')(sequelize,DataTypes)
+  db.product = require('./product')(sequelize,DataTypes)
+  db.userAddress = require('./userAddress.')(sequelize,DataTypes)
+  db.userCart = require('./userCart')(sequelize,DataTypes)
+
+
+
+  db.category.hasMany( db.product,{foreignKey:'category_id'})
+  db.product.belongsTo(db.category,{ foreignKey: 'category_id' })
+
+  
+  db.userModels.hasMany( db.userAddress,{foreignKey:'user_id'})
+  db.userAddress.belongsTo(db.userModels,{ foreignKey: 'user_id' })
+
+  db.userModels.hasMany( db.userCart,{foreignKey:'user_id'})
+  db.userCart.belongsTo(db.userModels,{ foreignKey: 'user_id' })
+
+  db.product.hasMany( db.userCart,{foreignKey:'product_id'})
+  db.userCart.belongsTo(db.product,{ foreignKey: 'product_id' })
+
+
+
+  sequelize.sync({force:false})
+
+  module.exports = db
